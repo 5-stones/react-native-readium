ccba9c46c7ee7f034713d3ea2f1d90db641d7dcc
Jacob Spizziri <jspizziri@weare5stones.com>
2022-03-29 13:27:21 -0500

    chore(example): clean up unnecessary testing code in the example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { enableScreens } from 'react-native-screens';
-
-enableScreens(false);
 
 import Reader from './Reader';
 
