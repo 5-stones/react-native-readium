d55a457b71213de4536294709788b50b74076622
Jacob Spizziri <jspizziri@weare5stones.com>
2022-12-07 15:28:22 -0500

    feat(web): add basic web implementation and example

---
+++ src/web/hooks/useSettingsObserver.ts
@@ -0,0 +1,21 @@
+import { useDeepCompareEffect } from 'use-deep-compare';
+import type D2Reader from '@d-i-t-a/reader';
+import type { UserSettings } from '@d-i-t-a/reader/dist/types/model/user-settings/UserSettings';
+
+import type { Settings } from '../../interfaces';
+
+export const useSettingsObserver = (
+  reader?: D2Reader | null,
+  settings?: Partial<Settings> | null,
+) => {
+  useDeepCompareEffect(() => {
+    if (reader && settings) {
+      // @ts-ignore - FIXME need to do some deeper investigation if these
+      // are equivalent or not
+      reader?.applyUserSettings(settings as Partial<UserSettings>);
+    }
+  }, [
+    settings,
+    !!reader,
+  ]);
+};
