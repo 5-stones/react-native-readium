5b74a37bf49fc10a02fd93023df7a5f408c1e913
Jacob Spizziri <jspizziri@weare5stones.com>
2026-01-27 15:26:57 -0500

    whoops

--- web/hooks/useDecorationsObserver.ts
+++ web/hooks/useDecorationsObserver.ts
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import type { EpubNavigator } from '@readium/navigator-web';
+import type { EpubNavigator } from '@readium/navigator';
 import type {
   DecorationGroups,
   DecorationActivatedEvent,
